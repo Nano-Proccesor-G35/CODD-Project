80e2917266681a4eee55afb65f7f2071011acfba
Kavindu Ranathunga <75308518+kavinduranathunga@users.noreply.github.com>
2026-04-29 23:43:59 +0530

    Add files via upload

---
+++ Version 2/Extra_com_TB/TB_Multiplier_4.vhd
@@ -0,0 +1,109 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/03/2026 02:29:46 PM
+-- Design Name: 
+-- Module Name: TB_Multiplier_4 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity TB_Multiplier_4 is
+--  Port ( );
+end TB_Multiplier_4;
+
+architecture Behavioral of TB_Multiplier_4 is
+
+COMPONENT Multiplier_4
+    PORT  (
+        A: IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        B: IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        Y: OUT STD_LOGIC_VECTOR(7 DOWNTO 0) );
+ END COMPONENT;
+ 
+ SIGNAL A,B : STD_LOGIC_VECTOR(3 DOWNTO 0);
+ SIGNAL Y: STD_LOGIC_VECTOR(7 DOWNTO 0);
+ 
+
+begin
+UUT : Multiplier_4 PORT MAP (
+    A=>A,
+    B=>B,
+    Y=>Y);
+    
+PROCESS BEGIN
+ 
+ -- 111 0101 0101 1010 0101
+    A<="0101";
+    B<="1010";
+
+-- random 
+ WAIT FOR 100ns;
+    
+    A<="1010";
+    B<="1001";
+
+    WAIT FOR 100ns;
+
+    A<="0011";
+
+    WAIT FOR 100ns;
+
+    B<="0001";
+
+    WAIT FOR 100ns;
+
+    A<="0111";
+
+    WAIT FOR 100ns;
+
+    B<="0110";
+
+    WAIT FOR 100ns;
+
+    A<="1011";
+
+    WAIT FOR 100ns;
+
+    B<="1010";
+
+    WAIT FOR 100ns;
+
+    A<="1111";
+
+    WAIT FOR 100ns;
+
+    B<="1101";
+
+    WAIT FOR 100ns;
+
+    B<="1111";
+
+    WAIT;
+
+END PROCESS;
+
+end Behavioral;
