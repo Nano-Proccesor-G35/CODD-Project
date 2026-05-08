1130302624ab5bf9c9589937edf3a36b86d67bdf
Kavindu Ranathunga <75308518+kavinduranathunga@users.noreply.github.com>
2026-05-08 15:42:01 +0530

    Update AdderSubtracter.vhd

--- Version 2/src/new/AdderSubtracter.vhd
+++ Version 2/src/new/AdderSubtracter.vhd
@@ -1,3 +1,24 @@
+----------------------------------------------------------------------------------
+-- Company: UOM CSE
+-- Engineer: 240549F
+-- 
+-- Create Date: 05/07/2026 03:27:39 AM
+-- Design Name: 
+-- Module Name: AdderSubtracter - Behavioral
+-- Project Name: NanoProcessor
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -12,27 +33,28 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity AdderSubtracter is
     Port ( A : in STD_LOGIC_Vector(3 downto 0);
-           B : in STD_LOGIC_vector(3 downto 0);
-           C_in : in STD_LOGIC_vector(1 downto 0);
-           S : out STD_LOGIC_vector(3 downto 0);
-           C_out : out STD_LOGIC;
-           Zero : out std_logic;
-           C_out_plus : out std_logic);
+       B : in STD_LOGIC_vector(3 downto 0);
+       C_in : in STD_LOGIC_vector(1 downto 0);
+       S : out STD_LOGIC_vector(3 downto 0);
+       C_out : out STD_LOGIC;
+       Zero : out std_logic;
+       C_out_plus : out std_logic);
 end AdderSubtracter;
 
 architecture Behavioral of AdderSubtracter is
 
 component FA 
- port ( 
- A: in std_logic; 
- B: in std_logic; 
- C_in: in std_logic; 
- S: out std_logic; 
- C_out: out std_logic); 
- end component; 
- 
- SIGNAL FA0_S, FA0_C, FA1_S, FA1_C, FA2_S, FA2_C, FA3_S, FA3_C , output, C_tmp : std_logic; 
-signal B_out,S0 : std_logic_vector(3 downto 0);
+    port ( 
+        A: in std_logic; 
+        B: in std_logic; 
+        C_in: in std_logic; 
+        S: out std_logic; 
+        C_out: out std_logic); 
+end component; 
+
+SIGNAL FA0_S, FA0_C, FA1_S, FA1_C, FA2_S, FA2_C, FA3_S, FA3_C , output, C_tmp : std_logic; 
+SIGNAL B_out,S0 : std_logic_vector(3 downto 0);
+
 begin
 B_out(0) <= (C_in(0) and (not C_in(1))) xor B(0);
 B_out(1) <= (C_in(0) and (not C_in(1))) xor B(1);
@@ -41,38 +63,39 @@ B_out(3) <= (C_in(0) and (not C_in(1))) xor B(3);
 
 C_tmp <= (C_in(0) and (not C_in(1)));
 
- FA_0 : FA 
-    port map ( 
-        A => A(0), 
-        B => B_out(0), 
-        C_in => C_tmp, 
-        S => S0(0), 
-        C_Out => FA0_C); 
- FA_1 : FA 
-    port map ( 
-        A => A(1), 
-        B => B_out(1), 
-        C_in => FA0_C, 
-        S => S0(1), 
-        C_Out => FA1_C);
- FA_2 : FA 
-     port map ( 
-         A => A(2), 
-         B => B_out(2), 
-         C_in => FA1_C, 
-         S => S0(2), 
-         C_Out => FA2_C); 
- FA_3 : FA 
-     port map ( 
-         A => A(3), 
-         B => B_out(3), 
-         C_in => FA2_C, 
-         S => S0(3), 
-         C_Out => output); 
-         
+FA_0 : FA 
+port map ( 
+    A => A(0), 
+    B => B_out(0), 
+    C_in => C_tmp, 
+    S => S0(0), 
+    C_Out => FA0_C); 
+FA_1 : FA 
+port map ( 
+    A => A(1), 
+    B => B_out(1), 
+    C_in => FA0_C, 
+    S => S0(1), 
+    C_Out => FA1_C);
+FA_2 : FA 
+ port map ( 
+     A => A(2), 
+     B => B_out(2), 
+     C_in => FA1_C, 
+     S => S0(2), 
+     C_Out => FA2_C); 
+FA_3 : FA 
+ port map ( 
+     A => A(3), 
+     B => B_out(3), 
+     C_in => FA2_C, 
+     S => S0(3), 
+     C_Out => output); 
+     
 
 C_Out <= C_tmp AND (output xor FA2_C) ;
 C_out_plus<= output and not(C_tmp);
+
 Zero <= (NOT(C_in(1) OR C_in(0))) AND (not(S0(0) or S0(1) or S0(2) or S0(3)));
 S<= S0;
 
